443fbfc299b8bd769e572cc8785b8769d988b60e
Noorain.Ansari <noorainsnz0786@gmail.com>
2024-08-02 10:50:21 +0530

    just posting the first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <p className='font-bold'>HI</p>
+  )
+}
+
+export default App
